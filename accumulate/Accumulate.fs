7c4160ba647710fba8defddf9c0981e3ab9ac654
daveyshindig <davey.wilkie@gmail.com>
2017-08-07 16:38:21 -1000

    Finished 2 more fsharp exercisms

---
+++ accumulate/Accumulate.fs
@@ -0,0 +1,8 @@
+﻿module Accumulate
+
+let accumulate (f : 'a -> 'a) (ls : 'a list) : 'a list =
+    let rec accumulate' f ls acc =
+        match ls with 
+        | [] -> List.rev acc
+        | hd :: tl -> accumulate' f tl (f hd :: acc)
+    accumulate' f ls []
---
+++ grade-school/GradeSchool.fs
@@ -0,0 +1,16 @@
+﻿module GradeSchool
+
+let empty = Map.empty
+
+let add (name : string) (grade : int) (school : Map<int,string list>) : Map<int,string list> =
+    match Map.tryFind grade school with
+    | None -> Map.add grade [name] school
+    | Some ls -> Map.add grade (List.sort (name :: ls)) school
+
+let grade (num : int) (school : Map<int, string list>) : string list =
+    match Map.tryFind num school with
+    | None -> []
+    | Some ls -> ls
+
+let roster (school : Map<int, string list>) : (int * string list) list =
+    Map.toList school
