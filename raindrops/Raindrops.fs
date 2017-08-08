775ccc9ec5b2a0440d33e81af4c8cb6ee62c4e4f
daveyshindig <davey.wilkie@gmail.com>
2017-08-07 14:49:03 -1000

    Finished two new exercises

---
+++ raindrops/Raindrops.fs
@@ -0,0 +1,6 @@
+﻿module Raindrops
+
+let convert (x : int) = 
+    let factors = [ (3, "Pling"); (5, "Plang"); (7, "Plong"); ]
+    let str = String.concat "" (seq { for i in factors do if x % fst i = 0 then yield snd i })
+    if str = "" then string x else str
---
+++ space-age/SpaceAge.fs
@@ -0,0 +1,23 @@
+﻿module SpaceAge
+
+type Planet =
+  | Earth
+  | Mercury
+  | Venus
+  | Mars
+  | Jupiter
+  | Saturn
+  | Uranus
+  | Neptune
+
+let spaceAge (planet : Planet) (secs : decimal) : decimal =
+    let earthYears = secs / 31557600m
+    match planet with
+    | Earth -> System.Math.Round(earthYears, 2)
+    | Mercury -> System.Math.Round(earthYears / 0.2408467m, 2)
+    | Venus -> System.Math.Round(earthYears / 0.61519726m, 2)
+    | Mars -> System.Math.Round(earthYears / 1.8808158m, 2)
+    | Jupiter -> System.Math.Round(earthYears / 11.862615m, 2)
+    | Saturn -> System.Math.Round(earthYears / 29.447498m, 2)
+    | Uranus -> System.Math.Round(earthYears / 84.016846m, 2)
+    | Neptune -> System.Math.Round(earthYears / 164.79132m, 2)
