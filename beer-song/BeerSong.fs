7fecce614851cf2ca4ffa5159206e2f5c42ef1a0
daveyshindig <davey.wilkie@gmail.com>
2017-08-08 14:09:32 -1000

    3 exercisms PAU!

---
+++ beer-song/BeerSong.fs
@@ -0,0 +1,22 @@
+﻿module BeerSong
+
+let verse x = 
+    let str s1 s2 s3 s4 = sprintf "%s of beer on the wall, %s of beer.\n\
+                           %s, %s of beer on the wall.\n\n" s1 s2 s3 s4
+    match x with
+    | 0 -> str "No more bottles" "no more bottles" 
+               "Go to the store and buy some more" "99 bottles"
+    | 1 -> str (string 1 + " bottle") (string 1 + " bottle")
+               "Take it down and pass it around" "no more bottles"
+    | y when y > 1 -> 
+        let lastbottle = if y - 1 = 1 then " bottle" else " bottles"
+        str (string y + " bottles") (string y + " bottles") 
+             "Take one down and pass it around" (string (y - 1) + lastbottle)
+    | _ -> ""
+
+let rec verses x y =
+    match x - y with
+    | 0 -> verse y
+    | _ -> verse x + verses (x - 1) y
+
+let sing = verses 99 0
---
+++ clock/Clock.fs
@@ -0,0 +1,19 @@
+﻿module Clock
+
+let mkClock h m = (h % 24 + m / 60, m % 60)
+
+let display clock = sprintf "%02i:%02i" (fst clock) (snd clock)
+
+let add m clock = 
+    let h = (fst clock + m / 60 ) % 24
+    let m = (snd clock + m % 60) % 60
+    let overflow = if snd clock + m % 60 > 59 then 1 else 0
+    (h + overflow) % 24, m
+
+let subtract m clock =
+    let m' = snd clock - m % 60
+    let overflow = if m' < 0 then 1 else 0
+    let m'' = if m' < 0 then m' + 60 else m'
+    let h' = fst clock - m / 60 - overflow
+    let h'' = if h' < 0 then h' + 24 else h'
+    h'', m''
