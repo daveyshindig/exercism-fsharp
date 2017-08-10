584e9e3788a234d2620402f0eaefa821d28f2cac
daveyshindig <davey.wilkie@gmail.com>
2017-08-10 13:15:36 -1000

    Finished 2 exercisms

---
+++ queen-attack/QueenAttack.fs
@@ -0,0 +1,5 @@
+﻿module QueenAttack
+
+let canAttack ((wx, wy): int * int) ((bx, by): int * int) =  
+    if wx = bx && wy = by then raise (System.FormatException "Queens cannot occupy same space")
+    else wx = bx || wy = by || System.Math.Abs (wx - bx) = System.Math.Abs (wy - by)    
---
+++ robot-simulator/RobotSimulator.fs
@@ -0,0 +1,36 @@
+﻿module RobotSimulator
+
+type Bearing = North | East | South | West
+
+let createRobot bearing (x, y) = (bearing, (x, y))
+
+let turnRight robot =
+    match robot with
+    | (North, (x, y)) -> (East, (x, y))
+    | (East, (x, y)) -> (South, (x, y))
+    | (South, (x, y)) -> (West, (x, y))
+    | (West, (x, y)) -> (North, (x, y))
+
+let turnLeft robot =
+    match robot with
+    | (North, (x, y)) -> (West, (x, y))
+    | (East, (x, y)) -> (North, (x, y))
+    | (South, (x, y)) -> (East, (x, y))
+    | (West, (x, y)) -> (South, (x, y))
+
+let advanceRobot robot =
+    match robot with
+    | (North, (x, y)) -> (North, (x, y + 1))
+    | (East, (x, y)) -> (East, (x + 1, y))
+    | (South, (x, y)) -> (South, (x, y - 1))
+    | (West, (x, y)) -> (West, (x - 1, y))
+
+let simulate robot cmds = 
+    let ls = [for c in cmds -> c]
+    let rec simrec r cmds =
+        match cmds with
+        | [] -> r
+        | 'R' :: tl -> simrec (turnRight r) tl
+        | 'L' :: tl -> simrec (turnLeft r) tl
+        | 'A' :: tl -> simrec (advanceRobot r) tl
+    simrec robot ls
