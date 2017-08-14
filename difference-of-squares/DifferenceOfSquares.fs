4b449a46ac8e690913bba26c6fc172068eb10e2d
daveyshindig <davey.wilkie@gmail.com>
2017-08-14 09:51:53 -1000

    2 exers

---
+++ difference-of-squares/DifferenceOfSquares.fs
@@ -0,0 +1,9 @@
+﻿module DifferenceOfSquares
+
+let square x = pown x 2
+
+let squareOfSums n = List.sum [1..n] |> square
+
+let sumOfSquares n = List.sumBy square [1..n]
+
+let difference x = System.Math.Abs (sumOfSquares x - squareOfSums x)
---
+++ pig-latin/PigLatin.fs
@@ -0,0 +1,17 @@
+﻿module PigLatin
+
+let translate (str: string) = 
+    let words = str.Split ' '
+    let trans (word: string) =
+        if word.[0..2] = "thr" || word.[0..2] = "sch" 
+        then word.[3..] + word.[0..2] + "ay"
+        elif word.[1..2] = "qu"
+        then word.[3..] + word.[0..2] + "ay"
+        elif word.[0..1] = "yt" || word.[0..1] = "xr"
+        then word + "ay"
+        elif word.[0..1] = "ch" || word.[0..1] = "qu" || word.[0..1] = "th" 
+        then word.[2..] + word.[0..1] + "ay"
+        else match word.[0] with
+        | 'a' | 'e' | 'i' | 'o' | 'u' -> word + "ay"
+        | _ -> word.[1..] + string word.[0] + "ay" 
+    Array.reduce (fun str1 str2 -> str1 + " " + str2) (Array.map trans words)
