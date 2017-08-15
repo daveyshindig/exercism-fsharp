3729270e2407c273abaf320390a283a01df6ea0e
daveyshindig <davey.wilkie@gmail.com>
2017-08-15 12:47:53 -1000

    2 (trivial) exercises

---
+++ gigasecond/Gigasecond.fs
@@ -0,0 +1,4 @@
+﻿module Gigasecond
+
+let gigasecond (date: System.DateTime) : System.DateTime =
+    date.Add(System.TimeSpan.FromSeconds(1000000000.)).Date
---
+++ rna-transcription/RNATranscription.fs
@@ -0,0 +1,11 @@
+﻿module RNATranscription
+
+let toRna str = 
+    let transcribe c = 
+        match c with
+        | 'C' -> 'G'
+        | 'G' -> 'C'
+        | 'T' -> 'A'
+        | 'A' -> 'U'
+        | _ -> failwith "Invalid DNA sequence"
+    System.String.Concat [for c in str do yield transcribe c]
