072e2842e27697d2956d81c851c8f4da76c9d1e2
daveyshindig <davey.wilkie@gmail.com>
2017-08-27 10:32:51 -1000

    Yay, two more

---
+++ secret-handshake/SecretHandshake.fs
@@ -0,0 +1,13 @@
+﻿module SecretHandshake
+
+let rec intToBinary x =
+    match x with
+    | 0 | 1 -> string x
+    | _ -> intToBinary (x / 2) + string (x % 2)
+
+let handshake (x: int) =
+    let strs = ["wink"; "double blink"; "close your eyes"; "jump"; "reverse"]
+    let nums = List.rev (List.ofSeq (intToBinary x))
+    let strs' = List.mapi (fun i x -> if x = '1' then strs.[i] else "") nums
+    if strs'.Length = 5 then List.tail (List.rev strs') else strs'
+    |> List.filter (fun str -> str <> "")
---
+++ word-count/Phrase.fs
@@ -0,0 +1,16 @@
+﻿module Phrase
+open System
+open System.Text.RegularExpressions
+
+let wordCount (phrase: string) =
+    let rec wc words map =
+        match words with
+        | [] -> map
+        | word :: tl when Map.containsKey word map -> 
+            let n = Map.find word map
+            wc tl (Map.add word (n+1) map)
+        | word :: tl -> wc tl (Map.add word 1 map)
+    let phrase' = Regex.Replace(phrase.ToLower(), "[.,!@#$%^&*:]| +'+|'$", " ")
+    let phrase'' = Regex.Replace(phrase', "\\s+", " ") 
+    let words = List.ofArray (phrase''.Split(' '))
+    wc words Map.empty |> Map.remove "" 
