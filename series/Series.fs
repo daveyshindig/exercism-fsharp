ca410c74673ada8dc94c18137181e111dded7e38
daveyshindig <davey.wilkie@gmail.com>
2017-08-30 11:33:55 -1000

    m0ar exerz

---
+++ series/Series.fs
@@ -0,0 +1,9 @@
+﻿module Series
+
+let charDigitMap = [('0', 0); ('1', 1); ('2', 2); ('3', 3); ('4', 4); 
+                    ('5', 5); ('6', 6); ('7', 7); ('8', 8); ('9', 9)] |> Map.ofList
+
+let slices (str: string) n = 
+    let arr = Seq.toArray str |> Array.map (fun x -> Map.find x charDigitMap)
+    if n > Array.length arr then failwith "N IS TOO LONGGGGGG"
+    else Array.mapi (fun i _ -> arr.[i .. i + n - 1] |> Array.toList) arr.[0  .. Array.length arr - n]
---
+++ simple-linked-list/SimpleLinkedList.fs
@@ -0,0 +1,42 @@
+﻿module SimpleLinkedList
+
+type SinglyLinkedList<'T> =
+    | Nil
+    | ListNode of 'T * SinglyLinkedList<'T>
+
+let nil = Nil
+
+let isNil sll = sll = Nil
+
+let create t ls = ListNode (t, ls)
+
+let datum (ls: SinglyLinkedList<'T>) =
+    match ls with
+    | Nil -> failwith "NO DATA"
+    | ListNode (t, _) -> t
+
+let next (ls: SinglyLinkedList<'T>) =
+    match ls with
+    | Nil -> failwith "NO DATA"
+    | ListNode (_, ls) -> ls
+
+let toList ls =
+    let rec toListRec ls acc = 
+        match ls with
+        | Nil -> acc
+        | ListNode (t, ls) -> toListRec ls (t :: acc)
+    toListRec ls [] |> List.rev
+
+let fromList ls =
+    let rec fromListRec ls acc = 
+        match ls with
+        | [] -> acc
+        | hd :: tl -> fromListRec tl (create hd acc)
+    fromListRec (ls |> List.rev) nil
+
+let reverse ls =
+    let rec reverseRec ls acc = 
+        match ls with
+        | Nil -> acc
+        | ListNode (t, ls) -> reverseRec ls (create t acc)
+    reverseRec ls nil
